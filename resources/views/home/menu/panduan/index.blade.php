73aebe33ef80e7e69c28aac154209720b5002efb
AryaWibawaZulfiansyah <arya08052000@gmail.com>
2024-06-18 11:01:48 +0700

    Tampilan Halaman Menu

---
+++ resources/views/home/menu/panduan/index.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMPN 2 Tasikmalaya</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .navbar-brand img {
+            height: 40px;
+        }
+
+        .navbar-nav {
+            margin-left: auto;
+        }
+
+        .menu-item {
+            background-color: #A8CD9F;
+            /* padding: 5px; */
+            padding-bottom: 20px;
+            border-radius: 5px;
+            /* margin: 10px 0; */
+            /* margin-right: 90px; */
+            /* height: 130px;
+            width: 130px; */
+            /* right: 10px; */
+            margin-bottom: 10px;
+            text-align: center;
+            transition: all 0.3s ease;
+
+        }
+
+        .menu-item a:link {
+            color: #4C4F69;
+            font-family: "Poppins", sans-serif;
+        }
+
+        .menu-item:hover {
+            background-color: #a9d7a7;
+            transform: translateY(-5px);
+        }
+
+        .menu-item i {
+            align-items: center;
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+
+        .menu-item svg {
+            color: white;
+        }
+
+        .main-content {
+            padding: 20px;
+        }
+
+        .login-card {
+            max-width: 800px;
+            /* margin: auto; */
+        }
+    </style>
+    @include('include.style')
+</head>
+
+<body>
+    <!-- Navbar -->
+    @include('include.content.navbar')
+    <div class="container-fluid" style="padding-left: 250px;">
+        <div class="row mt-4">
+            <!-- Left Menu Boxes -->
+            @include('home.menu.panduan.menu')
+            <!-- carousel -->
+            @include('include.content.home.carousel')
+            <!-- login -->
+            @include('include.content.home.login')
+        </div>
+    </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    @include('include.script')
+</body>
+
+</html>
---
+++ resources/views/home/menu/panduan/menu.blade.php
@@ -0,0 +1,49 @@
+<div class="col-md-4">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="menu-item" style="padding-top: 30px;">
+                <a href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+                    </svg><br>Home</a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="menu-item" style="padding-top: 30px;">
+                <a href="{{route('seleksi')}}"><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-signpost-split-fill" viewBox="0 0 16 16">
+                        <path d="M7 16h2V6h5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.8 2.4A1 1 0 0 0 14 2H9v-.586a1 1 0 0 0-2 0V7H2a1 1 0 0 0-.8.4L.225 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4h5z" />
+                    </svg><br> Seleksi</a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="menu-item" style="padding-top: 30px;">
+                <a href="{{route('biaya')}}"><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-wallet-fill" viewBox="0 0 16 16">
+                        <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542s.987-.254 1.194-.542C9.42 6.644 9.5 6.253 9.5 6a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2z" />
+                        <path d="M16 6.5h-5.551a2.7 2.7 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5s-1.613-.412-2.006-.958A2.7 2.7 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5z" />
+                    </svg><br> Biaya</a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="menu-item" style="padding-top: 30px; ">
+                <a href="{{route('beasiswa')}}"><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+                    </svg><br> Beasiswa</a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="menu-item" style="padding-top: 30px; background-color:#eff1f5">
+                <a href="{{route('panduan')}}"><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" style="color: #A8CD9F" class="bi bi-book-fill" viewBox="0 0 16 16">
+                        <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
+                    </svg><br> Panduan</a>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="menu-item" style="padding-top: 30px;">
+                <a href="{{route('berita')}}"><svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-newspaper" viewBox="0 0 16 16">
+                        <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z" />
+                        <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z" />
+                    </svg><br> Berita</a>
+            </div>
+        </div>
+    </div>
+</div>
