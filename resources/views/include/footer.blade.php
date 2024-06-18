0caf8d2edb41837a1544a3892eaa358ec97e0b5f
AryaWibawaZulfiansyah <arya08052000@gmail.com>
2024-06-18 11:06:25 +0700

    Include Other

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2024 &copy; SMPN 2 TASIKMALAYA</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class='text-danger'><i data-feather="heart"></i></span> by <a href="http://localhost">Heart</a></p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/include/script.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('templates/assets/js/feather-icons/feather.min.js')}}"></script>
+<script src="{{ asset('templates/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+<script src="{{ asset('templates/assets/js/app.js')}}"></script>
+
+<script src="{{ asset('templates/assets/vendors/chartjs/Chart.min.js')}}"></script>
+<script src="{{ asset('templates/assets/vendors/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{ asset('templates/assets/js/pages/dashboard.js')}}"></script>
+
+<script src="{{ asset('templates/assets/js/main.js')}}"></script>
